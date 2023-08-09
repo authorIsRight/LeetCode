9312f6f88be1a6c8ccb028c74e3f34a04cae488c
Илья Соболев <i4sobolev@yandex.ru>
2023-08-09 21:48:13 +0300

    longest common prefix zip plus set

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,51 @@
+"""Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters."""
+
+import json
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        a = ""
+        for i in zip(*strs):
+            if len(set(i)) == 1: 
+                print(i)
+                a += i[0]
+            else: 
+                return a
+        return a
+
+def read_input():
+
+    strs = json.loads(input())
+
+    return strs
+
+
+if __name__ == "__main__":
+    strs = read_input()
+    obj = Solution()
+    to_print = obj.longestCommonPrefix(strs)
+    print(to_print)
---
+++ tests.py
@@ -0,0 +1,7 @@
+#random bullshit go 
+
+i = 10
+text: str = "привет"
+print(text[0:2])
+print(text.startswith(text[0:i]))
+print(help(zip))
