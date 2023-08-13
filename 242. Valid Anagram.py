91fd191320a72000f4780f6cbbc45dd7471bb9e5
Илья Соболев <svetonosec94@gmail.com>
2023-08-13 16:56:04 +0300

    anagram beats 55 20

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,42 @@
+"""Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters."""
+
+import sys
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
+
+
+def read_input():
+    s = sys.stdin.readline()
+    t = sys.stdin.readline()
+    return s, t
+
+
+if __name__ == '__main__':
+    s, t = read_input()
+    obj = Solution()
+
+    print(obj.isAnagram(s, t))
