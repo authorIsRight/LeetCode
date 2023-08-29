45ee1fdfe025577c66c92fb465c3e97e384ac4ba
Илья Соболев <svetonosec94@gmail.com>
2023-08-30 00:15:14 +0300

    44 94

---
+++ 1768. Merge Strings Alternately.py
@@ -0,0 +1,58 @@
+"""You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters."""
+
+from itertools import zip_longest
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = ''.join(
+            c1+c2 for c1, c2 in zip_longest(word1, word2, fillvalue=""))
+        remaining = word1[len(merged):] + word2[len(merged):]
+        return merged+remaining
+
+
+def read_input():
+    word1 = input()
+    word2 = input()
+    return word1, word2
+
+
+if __name__ == "__main__":
+    word1, word2 = read_input()
+    obj = Solution()
+    print(obj.mergeAlternately(word1, word2))
